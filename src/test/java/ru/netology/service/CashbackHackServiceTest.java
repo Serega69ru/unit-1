505e6d9fa24ce92afd66f3ce25aa3514bc3b002b
Sergey Ivanov <ivanovsa69@yandex.ru>
2024-01-26 22:32:50 +0300

    add change testng

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -9,7 +9,7 @@ public class CashbackHackServiceTest {
         int amount = 1000;
         int actual = service.remain(amount);
         int expected = 0;
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
     @org.testng.annotations.Test
     public void testRemainWithCashback(){
@@ -17,7 +17,7 @@ public class CashbackHackServiceTest {
         int amount = 900;
         int actual = service.remain(amount);
         int expected = 100;
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 
 }
